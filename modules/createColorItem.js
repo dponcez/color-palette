172db2065470d517e74f0d0cb68a4867ce0cc51c
Damian Ponce <dponcez_git@hotmail.com>
2024-09-24 21:47:12 -0500

    refactor: change variable names and add try/catch method in eventHandler function

--- modules/createColorItem.js
+++ modules/createColorItem.js
@@ -11,89 +11,102 @@ import { debounce } from "../utils/debounce.js"
 
 const eventHandler = ($, event, callback) => $.addEventListener(event, callback);
 const selector = (element) => document.querySelector(element);
+const log = (value) => console.log(value);
 
 export const initApp = () => {
-  const createColorItems = (color, description) => {
-    // palette content
+  const createColorItems = (color, description) =>  {
     const htmlRefs = {
       paletteContainer: createElementByClass('div', 'palette--container'),
-      colorName: createElementByClass('code', 'color--name'),
+      hexColor: createElementByClass('div', 'hex--color'),
+      infoContainer: createElementByClass('div', 'info--container'),
+      hexInfoContainer: createElementByClass('div', 'hex--info__container'),
+      nameInfo: createElementByClass('p', 'name--info'),
+      hexInfo: createElementByClass('code', 'hex--info'),
+      title: createElementByClass('p', 'title'),
       copyBtn: createElementByClass('button', 'copy--btn'),
-      infoContainer: createElementByClass('div', 'color--info__container'),
-      info: createElementByClass('p', 'info'),
       alertBox: createElementByClass('div', 'box'),
       message: createElementByClass('p', 'message'),
-      audio: createElementByClass('audio', 'player')
+      audio: createElementByClass('audio', '')
     }
-  
+
     const {
       paletteContainer,
-      colorName,
-      copyBtn,
+      hexColor,
       infoContainer,
-      info,
+      hexInfoContainer,
+      nameInfo,
+      hexInfo,
+      title,
+      copyBtn,
       alertBox,
       message,
       audio
     } = htmlRefs;
-  
-    const grid = selector('[data-grid]');
+
+    const grid = selector('[data-grid');
     const container = selector('.container');
-  
-    paletteContainer.style.backgroundColor = color;
-    colorName.textContent = color;
+
+    hexColor.style.backgroundColor = color;
+    hexInfo.textContent = color;
+    nameInfo.textContent = 'hex:'
+    title.textContent = description;
     copyBtn.textContent = 'copy color';
-    info.textContent = description;
-  
-    if(color === '#000000'){
-      colorName.style.color = '#f5f6f7'
-    }
+
+    // append elements
+    hexInfoContainer.appendChild(nameInfo);
+    hexInfoContainer.appendChild(hexInfo);
+
+    infoContainer.appendChild(hexInfoContainer);
+    infoContainer.appendChild(title);
+
+    paletteContainer.appendChild(hexColor);
+    paletteContainer.appendChild(infoContainer);
+    paletteContainer.appendChild(copyBtn);
 
     // event
     eventHandler(copyBtn, 'click', debounce( async () => {
-      await navigator.clipboard.writeText(colorName.textContent);
 
-      message.textContent = 'copied!';
-
-      alertBox.appendChild(message);
-      container.appendChild(alertBox);
-
-      audio.src = '../assets/sound/sound.mp3';
-
-      if(alertBox.classList.contains('box')){
-        audio.play()
+      try {
+        await navigator.clipboard.writeText(colorName.textContent);
+  
+        message.textContent = 'copied!';
+  
+        alertBox.appendChild(message);
+        container.appendChild(alertBox);
+  
+        audio.src = '../assets/sound/sound.mp3';
+  
+        if(alertBox.classList.contains('box')){
+          audio.play()
+        }
+  
+        setTimeout(() => {
+          container.removeChild(alertBox);
+        }, 1500)
+        
+      } catch (error) {
+        log(`Error to copy the hex color: ${error}`)
       }
-
-      setTimeout(() => {
-        container.removeChild(alertBox);
-      }, 1500)
       
     }, false))
-  
-    // append elements
-    infoContainer.appendChild(info)
-  
-    paletteContainer.appendChild(colorName);
-    paletteContainer.appendChild(copyBtn);
-    paletteContainer.appendChild(infoContainer);
-  
+
     grid.appendChild(paletteContainer);
   }
-  
-  
+
   const fetchData = async () => {
     const requestUrl = '../json/index.json';
+
     try {
       const response = await fetch(requestUrl);
       const data = await response.json();
-    
-      for(const {color, description} of data){
+
+      for(const { color, description } of data){
         createColorItems(color, description)
       }
     } catch (error) {
-      console.log(error.message)
+      log(`Failure to parse the data: ${error}`)
     }
   }
-  
+
   fetchData()
 }
